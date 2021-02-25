e98dac6319bec23969f200ee3d3086763a04c2cc
wrallee <wrallee@naver.com>
2021-02-26 00:30:11 +0900

    maven-modularization

--- ingredient-service/src/test/java/tacos/IngredientServiceApplicationTests.java
+++ ingredient-service/src/test/java/tacos/IngredientServiceApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ServiceRegistryApplicationTests {
+class IngredientServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
