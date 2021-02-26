da169dca02588fb07263a1ae398ec8cd3b9aa248
wrallee <wrallee@naver.com>
2021-02-26 20:14:00 +0900

    add client-service

--- ingredient-service/src/main/java/tacos/IngredientServiceApplication.java
+++ ingredient-service/src/main/java/tacos/IngredientServiceApplication.java
@@ -2,11 +2,17 @@ package tacos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@EnableEurekaClient
+@RestController
 public class IngredientServiceApplication {
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello World";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(IngredientServiceApplication.class, args);
