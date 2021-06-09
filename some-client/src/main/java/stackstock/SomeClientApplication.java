8ab6e00a9e728349c5534f03402336d6323389af
wrallee <wrallee@naver.com>
2021-06-09 14:58:49 +0900

    fix(client): 애플리케이션 컴파일 오류 수정

--- some-client/src/main/java/stackstock/SomeClientApplication.java
+++ some-client/src/main/java/stackstock/SomeClientApplication.java
@@ -2,9 +2,7 @@ package stackstock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
-@EnableHystrixDashboard
 @SpringBootApplication
 public class SomeClientApplication {
 
